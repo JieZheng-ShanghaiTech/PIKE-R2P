54df466e8a35120078eccd752f5b3d7fdd2fc3eb
DDigimon <965790989@qq.com>
2021-02-15 17:19:28 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,2 @@
 python  main.py --mode test --name pbmc --type_list combine_database_text_experiment_coe --model_save_root ./results6/ --device cuda:1
+python  main.py --mode test --name cbmc --type_list combine_database_text_experiment_coe --model_save_root ./results6/ --device cuda:1
